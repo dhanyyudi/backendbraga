596cc28870c9b257a98cddee1a7671b702a0dc21
dhanyyudi <dhanyyudi2018@mail.ugm.ac.id>
2021-04-15 10:51:30 +0700

    day 2

---
+++ index.js
@@ -0,0 +1,41 @@
+// Require the framework and instantiate it
+const fastify = require("fastify")({ logger: true });
+const path = require("path");
+
+// Register plugins
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/",
+});
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public/assets"),
+  prefix: "/assets",
+  decorateReply: false,
+});
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public/forms"),
+  prefix: "/forms",
+  decorateReply: false,
+});
+
+// Declare a route
+fastify.get("/", async (request, reply) => {
+  reply.sendFile("index.html"); // serving path.join(__dirname, 'public', 'index.html') directly
+});
+
+// Declare a route
+fastify.get("/apaya", async (request, reply) => {
+  return { hello: "apaya" };
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
